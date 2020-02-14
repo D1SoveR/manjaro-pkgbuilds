e431fa5b1f1679b6e854cafd280db01b3783ca78
Mikołaj "D1SoveR" Banasik <d1sover@gmail.com>
2020-02-14 16:15:14 +0000

    Added setup for kernel configuration fragments.
    Using it to restore preemptive kernel config (as with the ZFS patch, it can be built against completely preemptive kernels).

--- local-repo-manager/packages/linux55-d1s/prepare.sh
+++ local-repo-manager/packages/linux55-d1s/prepare.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
+set -e
 
 # Clone Manjaro's PKGBUILD for Linux 5.5 kernel
 git clone --depth 1 https://gitlab.manjaro.org/packages/core/linux55.git .
+
+# Check for the merge config script (needed to apply the configuration fragments),
+# and throw an error if it can't be found
+merge_script="$(pacman -Qql | grep 'merge_config\.sh' | sort -r | head -1)"
+if [ -z "$merge_script" ]; then
+  echo "Could not find any versions of merge_config.sh, cannot continue the build" 1>&2
+  exit 1
+fi
 
 # Copy over the following patches to apply to kernel:
 # * Patch to un-GPL some kernel symbols to make ZFS module buildable again
 # * Valve's fsync patch, for improved performance in Windows games
 cp $prepare_dir/{zfs-fix,fsync}.patch .
 
+# Copy over the configuration fragment re-enabling fully preemptive kernel
+# (with the other patch in place we _can_ build ZFS module for one)
+cp $prepare_dir/preempt.config .
+
 # Apply required changes to PKGBUILD to customise package name and
 # ensure the patches above are applied
 patch -Np1 -i "$prepare_dir/pkgbuild-changes.patch"
+
+# Apply the patch that incorporates the configuration changes from given fragments
+# (replacing the placeholder "[merge_script]" with location of previously found
+#  merge_config.sh script)
+sed "s|\[merge_script\]|$merge_script|" "$prepare_dir/config.patch" | patch -Np1 -i -
